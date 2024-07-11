1d48fb209332589fc0931f42329e2b0badf6e1c7
Brian <woo.brian+ubuntudev@gmail.com>
2024-07-11 21:01:20 +0000

    deleted wrong naming

--- s06_flutter_dart_internals/android/app/src/main/kotlin/com/example/s06_flutter_dart_internals/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.s06_flutter_dart_internals
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- s06_understanding_keys/android/app/src/main/kotlin/com/example/s06_understanding_keys/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.s06_understanding_keys
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
